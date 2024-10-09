0c7b156c02e9f28fd2eff532e9b694cb878d29ef
Nexxxit <varsik.arsik@mail.ru>
2024-10-09 22:02:20 +0500

    Update Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,33 +1,96 @@
-import { Container, Navbar, Nav, Button } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Container, Navbar, Nav, Button, Modal } from "react-bootstrap";
+import { Link, useLocation } from "react-router-dom";
+import { useState } from "react";
 
 export default function Header() {
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  const [modalShow, setModalShow] = useState(false);
+  const location = useLocation();
+
+  const handleClickTheme = () => {
+    setIsDarkTheme(!isDarkTheme);
+  };
+
   return (
     <header>
       <Navbar className="bg-body-tertiary">
         <Container>
           <Navbar.Brand as={Link} to="/">
-            <img 
-            alt="" 
-            src="" 
-            width={30} 
-            height={30} 
-            className="d-inline-block align-top" 
-            />{' '}
+            <img
+              alt=""
+              src=""
+              width={30}
+              height={30}
+              className="d-inline-block align-top"
+            />{" "}
             Mint
           </Navbar.Brand>
           <Nav className="me-auto gap-2">
-            <Nav.Link as={Link} to="/" active>Dashboard</Nav.Link>
-            <Nav.Link as={Link} to="/transactions">Транзакции</Nav.Link>
-            <Nav.Link as={Link} to="/budgeting">Бюджетирование</Nav.Link>
-            <Nav.Link as={Link} to="/categories">Категории</Nav.Link>
+            <Nav.Link as={Link} to="/" active={location.pathname === '/'}>
+              Dashboard
+            </Nav.Link>
+            <Nav.Link as={Link} to="/transactions" active={location.pathname === '/transactions'}>
+              Транзакции
+            </Nav.Link>
+            <Nav.Link as={Link} to="/budgeting" active={location.pathname === '/budgeting'}>
+              Бюджетирование
+            </Nav.Link>
+            <Nav.Link as={Link} to="/categories" active={location.pathname === '/categories'}>
+              Категории
+            </Nav.Link>
           </Nav>
           <Nav className="gap-3">
-            <Button variant="link" className="text-dark fs-5" title="Настройки"><i className="bi bi-gear"></i></Button>
-            <Button variant="outline-secondary" className="fs-5" title="Выход"><i className="bi bi-door-closed text-dark"></i></Button>
+            <Button variant="light" className="fs-5" onClick={handleClickTheme}>
+              {isDarkTheme ? (
+                <i className="bi bi-brightness-high"></i>
+              ) : (
+                <i className="bi bi-moon"></i>
+              )}
+            </Button>
+            <Button
+              variant="light"
+              className="text-dark fs-5"
+              title="Настройки"
+            >
+              <i className="bi bi-gear"></i>
+            </Button>
+            <Button
+              variant="outline-secondary"
+              className="fs-5"
+              title="Выход"
+              onClick={() => setModalShow(true)}
+            >
+              <i className="bi bi-door-closed text-dark"></i>
+            </Button>
           </Nav>
         </Container>
       </Navbar>
+      <LeaveModal show={modalShow} onHide={() => setModalShow(false)} />
     </header>
+  );
+}
+
+function LeaveModal(props) {
+  return (
+    <Modal
+      {...props}
+      size="md"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header className="d-flex justify-content-center">
+        <Modal.Title id="contained-modal-title-vcenter">
+          Выход из аккаунта
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Footer className="d-flex justify-content-center">
+        <Button variant="secondary" onClick={props.onHide}>
+          Отмена
+        </Button>
+        <Button variant="danger" onClick={props.onHide}>
+          Выйти
+        </Button>
+      </Modal.Footer>
+    </Modal>
   );
 }
